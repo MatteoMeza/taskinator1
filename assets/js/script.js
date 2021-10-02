20f1664cb976754e64bb7f5329020e2a412e00cb
MatteoMeza <matteomeza@gmail.com>
2021-10-01 22:33:35 -0700

    capture form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,24 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
     event.preventDefault();
+    // adding .value gets the user input entered in the <input> tag and stores it in the taskNameInput variable
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    // same as above, but this gets the user selected option from the dropdown and stores it in the taskTypeInput variable
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+   
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 };
 

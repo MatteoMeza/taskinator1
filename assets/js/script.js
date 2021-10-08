0b1138aabb0e943238ad31f56e0f0b3d7053e012
MatteoMeza <matteomeza@gmail.com>
2021-10-07 20:24:07 -0700

    add data persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -225,6 +225,24 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+var loadTasks = function() {
+    // get task items from localStorage and store in savedTasks variable
+    var savedTasks = localStorage.getItem("tasks");
+    
+    if (!savedTasks) {
+        return false;
+    } 
+    // convert tasks from the string format back into an array of objects
+    savedTasks = JSON.parse(savedTasks);
+
+    // loop through savedTasks array
+    for (var i = 0; i < savedTasks.length; i++) {
+        // pass each task object into the `createTasksEl()` function
+        createTaskEl(savedTasks[i]);
+    }
+};
+
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+loadTasks();
